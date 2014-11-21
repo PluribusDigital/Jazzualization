b87a8dc8721a25f88ec57e6aa89748e804678ab6
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2014-11-21 08:37:59 -0500

    Support for multiple refreshes of graphics engine

--- app/js/use_case_force_controller.js
+++ app/js/use_case_force_controller.js
@@ -20,170 +20,212 @@ function dragstart(d) {
     'use strict';
 
     angular.module('jazzualization', [])
-      .controller('UseCaseForceController', function () {
-          /************************************************************************************************
-           * Properties
-           */
-          this.artifactTypes = ['', 'Use Case', 'Entity', 'Control', 'UI Pattern', 'Navigation', 'Key Screen', 'Actor', 'Role']
-          this.keywords = ['diagram', 'report', 'user', 'package:', 'auditing']
+      .controller('UseCaseForceController', ['$scope', '$http', '$location',
+          function ($scope, $http, $location) {
+              /************************************************************************************************
+               * Properties
+               */
+              $scope.artifactTypes = ['', 'Use Case', 'Entity', 'Control', 'UI Pattern', 'Navigation', 'Key Screen', 'Actor', 'Role']
+              $scope.keywords = ['diagram', 'report', 'user', 'package:', 'auditing']
 
-          this.width = Math.max(document.getElementById('view').offsetWidth, 960);
-          this.height = optimalHeight();
+              $scope.width = Math.max(document.getElementById('view').offsetWidth, 960);
+              $scope.height = optimalHeight();
 
-          this.radius = Math.sqrt(this.width * this.height) / 20;
-          this.charge = -1 * this.radius;
+              $scope.radius = Math.sqrt($scope.width * $scope.height) / 20;
+              $scope.charge = -1 * $scope.radius;
 
-          this.nodes = [];
-          this.links = [];
+              $scope.nodes = [];
+              $scope.links = [];
 
-          /************************************************************************************************
-           * d3js Properties
-           */
-          this.colors = d3.scale.category20();
+              /************************************************************************************************
+               * d3js Properties
+               */
+              $scope.colors = d3.scale.category20();
 
-          this.force = d3.layout.force()
-                         .size([this.width, this.height])
+              $scope.force = d3.layout.force()
+                             .size([$scope.width, $scope.height])
 
-          this.svg = d3.select(".view")
-                       .append("g");
+              $scope.svg = d3.select(".view")
+                           .append("g");
 
-          this.background = this.svg
-                                .append("rect")
-                                .attr("class", "underlay")
-                                .attr("width", this.width)
-                                .attr("height", this.height);
+              $scope.background = $scope.svg
+                                    .append("rect")
+                                    .attr("class", "underlay")
+                                    .attr("width", $scope.width)
+                                    .attr("height", $scope.height);
 
-          this.graphLayer = this.svg.append("g");
+              $scope.graphLayer = $scope.svg.append("g");
 
-          this.d3zoom = d3.behavior.zoom()
-                          .scaleExtent([.25, 8])
+              $scope.d3zoom = d3.behavior.zoom()
+                              .scaleExtent([.25, 8])
 
-          this.svgLinkEnum = this.graphLayer.selectAll(".link"),
-          this.svgNodeEnum = this.graphLayer.selectAll(".node");
+              $scope.svgLinkEnum = $scope.graphLayer.selectAll(".link"),
+              $scope.svgNodeEnum = $scope.graphLayer.selectAll(".node");
 
-          /************************************************************************************************
-          * Methods
-          */
-          this.getNode = function (id) {
-              return $.grep(this.nodes, function (x) { return x.id == id})[0]
-          }
+              /************************************************************************************************
+              * Commands
+              */
+              $scope.viewUseCases = function () {
+                  $http.get('data/uc.json').success($scope.onModelLoaded)
+              }
 
-          this.chooseColor = function(d) {
-              var index = this.artifactTypes.indexOf(d.type)
-              if (index < 1) {
-                  var s = d.name.toLowerCase().split(" ");
-                  for (var i = 0; i < s.length && index <= 0; i++) {
-                      index = this.keywords.indexOf(s[i])
+              $scope.viewUserInterface = function () {
+                  $http.get('data/ui.json').success($scope.onModelLoaded)
+              }
+
+              $scope.viewDataModel = function () {
+                  $http.get('data/dm.json').success($scope.onModelLoaded)
+              }
+
+              /************************************************************************************************
+              * Node Attribute Methods
+              */
+              $scope.nodeRX = function (d) {
+                  return $scope.radius * 1.3;
+              }
+
+              $scope.nodeRY = function (d) {
+                  return $scope.radius * 0.8;
+              }
+
+              $scope.nodeColor = function (d) {
+                  var index = $scope.artifactTypes.indexOf(d.type)
+                  if (index < 1) {
+                      var s = d.name.toLowerCase().split(" ");
+                      for (var i = 0; i < s.length && index <= 0; i++) {
+                          index = $scope.keywords.indexOf(s[i])
+                      }
+
+                      index + $scope.artifactTypes.length
                   }
 
-                  index + this.artifactTypes.length
+                  return $scope.colors(index);
               }
 
-              return this.colors(index);
-          }
-
-          this.onModelLoaded = function(data) {
-              this.nodes = data.nodes.slice();
-              this.links = data.links.slice();
-
-              // Update some settings based on the size of the data set
-              this.radius = Math.max(Math.sqrt(this.width * this.height) / (this.nodes.length * 2), this.radius);
-              this.charge = -0.5 * this.nodes.length * this.radius
-
-              this.applyFilter();
-          }
-
-          this.applyFilter = function () {
-              var filterNodes = this.nodes;
-              var filterLinks = [];
-
-              for (var i = 0; i < this.links.length; i++) {
-                  var link = this.links[i];
-                  var s = this.getNode(link.source),
-                      t = this.getNode(link.target)
-                  filterLinks.push({ source: s, target: t });
+              $scope.nodeKey = function (d) {
+                  return d.id;
               }
 
-              this.updateGraph(filterNodes, filterLinks);
-          }
+              /************************************************************************************************
+              * Layout Methods
+              */
+              $scope.getNode = function (id) {
+                  for (var i = 0; i < $scope.nodes.length; i++) {
+                      if ($scope.nodes[i].id == id)
+                          return $scope.nodes[i];
+                  }
 
-          this.updateGraph = function (nodes, links) {
-              // Restart the force layout.
-              this.force
-                  .nodes(nodes)
-                  .links(links)
-                  .linkDistance(3 * this.radius)
-                  .charge(this.charge)
-                  //.friction(0.99)
-                  .gravity(0.25)
-                  //.theta(0.1)
-                  .alpha(0.2)
-                  .start();
+                  return {};
+              }
 
-              // Update links.
-              this.svgLinkEnum = this.svgLinkEnum.data(links, function (d) { return d.target.id; });
+              $scope.onModelLoaded = function (data) {
+                  $scope.nodes = data.nodes.slice();
+                  $scope.links = data.links.slice();
 
-              this.svgLinkEnum.exit().remove();
+                  // Update some settings based on the size of the data set
+                  $scope.radius = Math.max(Math.sqrt($scope.width * $scope.height) / ($scope.nodes.length * 2), $scope.radius);
+                  $scope.charge = -0.5 * $scope.nodes.length * $scope.radius
 
-              this.svgLinkEnum.enter()
-                  .insert("line", ".node")
-                  .attr("class", "link");
+                  $scope.applyFilter();
+              }
 
-              // Update nodes.
-              this.svgNodeEnum = this.svgNodeEnum.data(nodes, function (d) { return d.id; });
+              $scope.applyFilter = function () {
+                  var n0 = $scope.force.nodes();
+                  var l0 = $scope.force.links();
 
-              this.svgNodeEnum.exit().remove();
+                  // Clear the links & nodes
+                  l0.splice(0, l0.length);
+                  n0.splice(0, n0.length);
 
-              var nodeEnter = this.svgNodeEnum.enter().append("g")
-                  .attr("class", "node")
-                  .on("click", click)
-                  .on("dblclick", dblclick)
-                  .call(this.force.drag);
+                  // Add the filtered nodes
+                  for (var n = 0; n < $scope.nodes.length; n++) {
+                      n0.push($scope.nodes[n]);
+                  }
 
-              var rx = this.radius * 1.3;
-              var ry = this.radius * 0.8;
+                  for (var i = 0; i < $scope.links.length; i++) {
+                      var link = $scope.links[i];
+                      var s = $scope.getNode(link.source),
+                          t = $scope.getNode(link.target)
+                      l0.push({ source: s, target: t });
+                  }
 
-              nodeEnter.append("ellipse")
-                  .attr("rx", function (d) { return rx; })
-                  .attr("ry", function (d) { return ry })
-                  .style("fill", this.chooseColor.bind(this))
+                  $scope.updateGraph();
+              }
 
-              nodeEnter.append("text")
-                  .attr("dy", ".35em")
-                  .text(function (d) { return d.name; });
-          }
+              $scope.updateGraph = function () {
+                  // Update links.
+                  //$scope.svgLinkEnum = $scope.svgLinkEnum.data($scope.force.links(), function (d) { return d.target.id; });
+                  $scope.svgLinkEnum = $scope.svgLinkEnum.data($scope.force.links());
+                  var lDeletes = $scope.svgLinkEnum.exit();
+                  var lInserts = $scope.svgLinkEnum.enter();
 
-          this.tick = function() {
-              this.svgLinkEnum.attr("x1", function (d) { return d.source.x; })
-                   .attr("y1", function (d) { return d.source.y; })
-                   .attr("x2", function (d) { return d.target.x; })
-                   .attr("y2", function (d) { return d.target.y; });
+                  lDeletes.remove();
+                  lInserts.insert("line", ".node").attr("class", "link");
 
-              this.svgNodeEnum.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
-          }
+                  // Update nodes.
+                  $scope.svgNodeEnum = $scope.svgNodeEnum.data($scope.force.nodes(), $scope.nodeKey);
+                  var nDeletes = $scope.svgNodeEnum.exit();
+                  var nInserts = $scope.svgNodeEnum.enter();
 
-          this.onZoom = function () {
-              //
-              this.graphLayer.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-          }
+                  nDeletes.remove();
 
-          /************************************************************************************************
-          * Initialize
-          */
+                  var nodeEnter = nInserts.append("g")
+                      .attr("class", "node")
+                      .on("click", click)
+                      .on("dblclick", dblclick)
+                      .call($scope.force.drag);
 
-          this.d3zoom.on("zoom", this.onZoom.bind(this));
+                  nodeEnter.append("ellipse")
+                      .attr("rx", $scope.nodeRX)
+                      .attr("ry", $scope.nodeRY)
+                      .style("fill", $scope.nodeColor)
 
-          this.force.drag().on("dragstart", dragstart);
-          this.force.on("tick", this.tick.bind(this));
+                  nodeEnter.append("text")
+                      .attr("dy", ".35em")
+                      .text(function (d) { return d.name; });
 
-          this.background.call(this.d3zoom);
+                  // Update the layout engine
+                  $scope.updateLayout();
+              }
 
+              $scope.updateLayout = function () {
+                  // Restart the force layout.
+                  $scope.force
+                      .linkDistance(3 * $scope.radius)
+                      .charge($scope.charge)
+                      //.friction(0.99)
+                      .gravity(0.25)
+                      //.theta(0.1)
+                      .alpha(0.2)
+                      .start();
+              }
 
-          $.ajax({
-              url: 'data/uc.json'
-          }).success(this.onModelLoaded.bind(this))
-          .fail(function (data) {
-             alert('doh');
-          });
-      });
+              $scope.tick = function () {
+                  $scope.svgLinkEnum.attr("x1", function (d) { return d.source.x; })
+                       .attr("y1", function (d) { return d.source.y; })
+                       .attr("x2", function (d) { return d.target.x; })
+                       .attr("y2", function (d) { return d.target.y; });
+
+                  $scope.svgNodeEnum.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
+              }
+
+              $scope.onZoom = function () {
+                  //
+                  $scope.graphLayer.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+              }
+
+              /************************************************************************************************
+              * Initialize
+              */
+              // Register event handlers
+              $scope.d3zoom.on("zoom", $scope.onZoom);
+              $scope.force.drag().on("dragstart", dragstart);
+              $scope.force.on("tick", $scope.tick);
+
+              // Start listening for zoom events
+              $scope.background.call($scope.d3zoom);
+
+              // Load the first file
+              $scope.viewUseCases()
+          }]);
 })(window.angular);
